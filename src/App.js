c52a0b117b94f7fce30637dee3af60af960b3d7b
NagaveniPusuluri <93608500+NagaveniPusuluri@users.noreply.github.com>
2022-10-18 21:00:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,164 @@
+/*import React, { useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
+
+export const API_KEY = "a9118a3a";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const MovieImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 15px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;
+`;
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
+
+function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+
+  const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
+
+  const [timeoutId, updateTimeoutId] = useState();
+
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `   );
+    updateMovieList(response.data.Search);
+  };
+
+  const onTextChange = (e) => {
+    onMovieSelect("")
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+  return (
+    <Container>
+      <Header>
+        <AppName>e src="/react-movie-app/movie-icon.svg" />
+          React Movie App
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="/react-movie-app/search-icon.svg" />
+          <SearchInput
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
+      <MovieListContainer>
+        {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+          <Placeholder src="/react-movie-app/movie-icon.svg" />
+        )}
+      </MovieListContainer>
+    </Container>
+  );
+}
+
+export default App;
+*/
+import React from 'react';
+import './App.css';
+import Img from './Images/profile.jpg';
+export default function App(){
+  return(
+    <div className='App'>
+    <div className='app-main'>
+      <header className='top'>
+        <div>        
+          <button className='back-button'>
+          Back
+          </button>
+          </div>
+          <div className='profile-main'>
+          <img className='profile' src={Img} alt=""/>
+          <h6 className='account-details' >Personal Account</h6>
+          <h4 className='mail'>Andrew@polika.io</h4>
+          
+        </div>
+      </header>
+      <div className='main'>
+        <div className='password'>
+        <h6 className='password-head'>Password</h6>
+        <input className='input-mail' type="mail" />
+        </div>
+        <p className='forgot-password'>Forgot Password? <span className='reset'>Reset</span></p>
+        </div>
+        <footer className='footer'>
+          <div className='footer-left'>
+          <input className='check-box'  type="checkBox" checked/>
+          <span className='check-text'>Stay signed in</span>
+          </div>
+          <button className="button">Sign in</button>
+        </footer>
+        </div>
+    </div>
+  )
+}
